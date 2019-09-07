5c81c3e0c313a81cd01855f173cbb7f6d103051e
Sid Shankar <sidshank@umich.edu>
2019-09-06 21:44:42 -0400

    Setting up the basics

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+})
+
+
+const {PORT} = process.env;
+app.listen(PORT, () => console.log(`Application started on port ${PORT}`))
