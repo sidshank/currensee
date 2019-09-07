7479e6954b014794a70e08497a181c8d9343d3c2
Sid Shankar <siddharth.shankar@gmail.com>
2019-09-07 06:30:36 -0400

    Database integration added (#1)

---
+++ server/db-conn.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const { DB_USER, DB_PW, DB_CONN, DB_NAME} = process.env
+
+console.log(DB_CONN)
+
+mongoose
+    .connect(
+        DB_CONN,
+        {auth: {user: DB_USER, password: DB_PW}, useNewUrlParser: true, dbName: DB_NAME}
+    )
+    .then(() => console.log("Successfully connected to the database"))
+    .catch(console.error)
