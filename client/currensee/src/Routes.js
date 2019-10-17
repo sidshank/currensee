8f2345ee367fe390a0f33113f1e1babdac30832d
Sid Shankar <siddharth.shankar@gmail.com>
2019-10-16 21:40:47 -0400

    Use react router for navbar routing within app, using LinkContainer (#5)
    * Use react router for navbar routing within app, using LinkContainer

--- client/currensee/src/Routes.js
+++ client/currensee/src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Upload from "./containers/Upload";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/upload/" exact component={Upload} />
     </Switch>
   );
 }
---
+++ client/currensee/src/containers/Upload.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Upload.css";
+
+export default function Home() {
+  return (
+    <div className="Upload">
+      <div className="lander">
+        <h1>Currensee</h1>
+        <p>Upload new currency notes here!</p>
+      </div>
+    </div>
+  );
+}
