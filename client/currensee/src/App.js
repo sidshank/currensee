8d6492f3c1537acf8fcd764b3972c85400574407
Sid Shankar <sidshank@umich.edu>
2019-10-16 21:38:27 -0400

    Use react router for navbar routing within app, using LinkContainer

--- client/currensee/src/App.js
+++ client/currensee/src/App.js
@@ -1,32 +1,28 @@
 import React from "react";
-import {
-  Button,
-  Navbar,
-  Nav,
-  Form,
-  FormControl
-} from "react-bootstrap";
+import { Navbar, Nav} from "react-bootstrap";
+import { Link } from "react-router-dom"
+import { LinkContainer } from "react-router-bootstrap";
 
 import Routes from "./Routes";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App(props) {
+function App(/*props*/) {
   return (
     <div className="App container">
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">
-          Currensee
+        <Navbar.Brand>
+          <Link to="/">Scratch</Link>
         </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/upload">Upload</Nav.Link>
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/upload/">
+            <Nav.Link>Upload</Nav.Link>
+          </LinkContainer>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
       </Navbar>
       <Routes />
     </div>
